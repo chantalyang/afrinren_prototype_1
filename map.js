1511f50f9eeff5be4903a2b52475653da1ed7036
Chantal <chantalyang17@gmail.com>
2015-09-07 21:23:57 +0200

    Add files

---
+++ map.js
@@ -0,0 +1,38 @@
+var map;
+var Google
+
+//Initialise Map Function
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 0.070959, lng: 23.923482}, //0.070959, 23.923482
+    zoom: 2,
+    streetViewControl: false,
+  });
+
+  /* For physical map overlay */
+  // http://jsfiddle.net/4cWCW/575/
+
+  //Setbounds of overlay
+  var imageBounds = new google.maps.LatLngBounds(
+      new google.maps.LatLng(-41.97938439964395, -38.127905909394485),
+      new google.maps.LatLng(55.107204794640154, 87.37057231783751));
+ 
+ //Overlay settings
+  cableOverlay = new google.maps.GroundOverlay(
+      'physical.png',
+      imageBounds);
+  cableOverlay.setOpacity(0.7)
+  cableOverlay.setMap(map);
+
+  /* Markers */ 
+  var marker = new google.maps.Marker({
+    position: {lat: 0.070959, lng: 23.923482},
+    title: "Marker"
+  });
+
+  marker.setMap(map);
+ // map.data.loadGeoJson("google.json");
+
+
+
+}//End initialise map function
