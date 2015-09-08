1115e75413cf61a014a0ee2ca81c69f08c22d492
Chantal <chantalyang17@gmail.com>
2015-09-08 10:50:27 +0200

    Clean up code and added comments

--- map.js
+++ map.js
@@ -1,7 +1,6 @@
 var map;
-var Google
 
-//Initialise Map Function
+// ------------- Initialise Map Function ------------ //
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 0.070959, lng: 23.923482}, //0.070959, 23.923482
@@ -9,10 +8,10 @@ function initMap() {
     streetViewControl: false,
   });
 
-  /*Add physical map overlay */
+  /* Setup physical map overlay */
   // http://jsfiddle.net/4cWCW/575/
 
-  //Setbounds of overlay
+  //Set bounds of overlay
   var imageBounds = new google.maps.LatLngBounds(
       new google.maps.LatLng(-41.97938439964395, -38.127905909394485),
       new google.maps.LatLng(55.107204794640154, 87.37057231783751));
@@ -24,28 +23,21 @@ function initMap() {
   cableOverlay.setOpacity(0.7)
   cableOverlay.setMap(map);
 
-  /*Add target_ip markers to map */ 
+  /*S etup target_ip Markers */ 
 
-  var circ = {
-    path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
-    fillColor: 'yellow',
-    fillOpacity: 0.8,
-    //scale: 1,
-    strokeColor: 'gold',
-    strokeWeight: 14
-  };
 
   map.data.loadGeoJson("/data/target_ips.json");
   map.data.setStyle({
   icon: "http://maps.google.com/mapfiles/ms/icons/orange-dot.png",
+  title: "Hello",
   clickable: true
   })
 
-//For debugging
+  //For debugging
    map.data.addListener('mouseover', function(event) {
     console.log(event.feature.getProperty("name"))
   });
 
 
 
-}//End initialise map function
+}// -------- End initialise map function ------------- //
