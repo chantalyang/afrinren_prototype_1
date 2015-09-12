c3eba74386231e4b21ed2cc0f93ee1dfc62fea8c
Chantal <chantalyang17@gmail.com>
2015-09-13 00:03:10 +0200

    Add measurement results json files

---
+++ data/measurements/extract_json.py
@@ -0,0 +1,23 @@
+import json
+jsonFile = open('41_223_156_170.json', 'r')
+parsed_json = json.load(jsonFile)
+jsonFile.close()
+
+print(parsed_json['lts'])
+
+
+def convert_json(items):
+    import json
+    return json.dumps({ "type": "FeatureCollection",
+                        "features": [ 
+                                        {"type": "Feature",
+                                         "geometry": { "type": "Point",
+                                                       "coordinates": [ feature['lon'],
+                                                                        feature['lat']]},
+                                         "properties": { key: value 
+                                                         for key, value in feature.items()
+                                                         if key not in ('lat', 'lon') }
+                                         } 
+                                     for feature in json.loads(items)
+                                    ]
+                       })
